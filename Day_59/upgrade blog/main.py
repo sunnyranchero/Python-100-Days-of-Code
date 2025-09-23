1d0bc39251d8777452ff1cdc2e5bdee1e9e5e07d
Sunny <cr.chappell@pm.me>
2025-09-23 17:44:27 -0400

    Finished up day 59, step 6

--- Day_59/upgrade blog/main.py
+++ Day_59/upgrade blog/main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+import requests
+from rich import print as rp
 
 app = Flask(__name__)
 
+API_ENDPOINT = "https://api.npoint.io/cadbb639003d1baeeaf6"
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    global API_ENDPOINT
+    full_json_content = requests.get(API_ENDPOINT).json()
+    rp(full_json_content)
+    return render_template("index.html", all_posts_json = full_json_content)
 
 @app.route("/about")
 def about():
@@ -15,6 +21,7 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 
 # Place this line at the bottom in order to run this directly
