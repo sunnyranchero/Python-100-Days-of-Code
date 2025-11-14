8df4b31639bc5c2928c6d3af28b0d2216bbb6bcd
Sunny <cr.chappell@pm.me>
2025-11-13 21:10:07 -0500

    Day 62: completed the add function and created an extra page for deletions

--- Day_62/main.py
+++ Day_62/main.py
@@ -29,7 +29,7 @@ Bootstrap5(app)
 (0?[1-9]|1[0-2]) matches hours from 01 to 12 (with optional leading zero for single-digit hours).
 : matches the literal colon.
 [0-5][0-9] matches valid minutes from 00 to 59.
-\s? allows for zero or one whitespace character between the time and AM/PM.
+\\s? allows for zero or one whitespace character between the time and AM/PM.
 (?:AM|PM|am|pm) is a non-capturing group matching the AM or PM indicator in any case.
 $ asserts the end of the string."""
 
@@ -66,6 +66,12 @@ class CafeForm(FlaskForm):
     # The submit is from the original.
     submit = SubmitField('Submit')
 
+class CafeFormDel(FlaskForm):
+    cafe = SelectField('Cafe name',
+                        validators=[DataRequired()])
+    # The submit is from the original.
+    submit = SubmitField('Submit')
+
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
@@ -85,14 +91,75 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-        # print(form)
+        new_row = [form.cafe.data,
+                   form.cafe_location.data, 
+                    form.opening_time.data,
+                    form.closing_time.data,
+                   form.coffee_rating.data, 
+                   form.wifi_strength_rating.data,
+                   form.power_outlet_rating.data]      
+
+        os.chdir(os.path.dirname(__file__))
+        with open('./cafe-data.csv', 'r', newline='\n', encoding='utf-8') as csv_file:
+            csv_data = csv.reader(csv_file, delimiter=',')
+        print(new_row)
+        with open('./cafe-data.csv', 'a', newline='\n', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(new_row)
         return redirect(url_for('add_cafe'))
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    print(os.getcwd())
+    # print(os.getcwd())
     return render_template('add.html', form=form)
+
+@app.route('/del', methods=['GET', 'POST'])
+def del_cafe():
+    os.chdir(os.path.dirname(__file__))
+    cafe_deletion_selection = ''
+    selected_row_data = []
+    form = CafeFormDel()   
+    
+    with open('./cafe-data.csv', 'r', newline='\n', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        # Creates a tuple, 1 is the lable, 1 is the value
+        choices_list = [(row[0],row[0]) for row in csv_data if 'Cafe Name' not in row[0]]
+        print(choices_list)
+    
+    form.cafe.choices = choices_list
+
+    if form.validate_on_submit():
+        cafe_deletion_selection = form.cafe.data
+        csv_new = []
+        with open('./cafe-data.csv', 'r', newline='\n', encoding='utf-8') as csv_file:
+            csv_data = csv.reader(csv_file, delimiter=',')
+            # Creates a tuple, 1 is the lable, 1 is the value
+            csv_rows = [row for row in csv_data]
+            csv_header = csv_rows[0]
+            
+            selected_row_data = [row for row in csv_rows if row[0] == cafe_deletion_selection]
+            csv_new = [row for row in csv_rows if row[0] != cafe_deletion_selection]
+            if csv_header not in csv_new:
+                csv_new.insert(0, csv_header)
+
+        with open('./cafe-data.csv', 'w', newline='\n', encoding='utf-8') as n:
+            writer = csv.writer(n)
+            writer.writerows(csv_new)
+            choices_list = [(row[0],row[0]) for row in csv_new if 'Cafe Name' not in row[0]]
+            form.cafe.choices = choices_list
+
+        # return redirect(url_for('del_cafe'))
+        return render_template('del.html', form=form,
+                                cafe_deleted=cafe_deletion_selection,
+                                selected_row_data=selected_row_data)
+    
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    # with   if form.validate_on_submit()
+    # print(os.getcwd())
+    return render_template('del.html', form=form,
+                           cafe_deleted=cafe_deletion_selection,
+                           selected_row_data=selected_row_data)
 
 
 @app.route('/cafes')
@@ -101,7 +168,7 @@ def cafes():
     # accessing the cwd/pwd for this project because of how I have it
     # being run. The solution is to change the dir to the executing script.
     os.chdir(os.path.dirname(__file__))
-    with open('./cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+    with open('./cafe-data.csv', 'r', newline='\n', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
