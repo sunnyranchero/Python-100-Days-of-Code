87897f5c2e34588b6bda1608dd826c3a4f173ff9
Sunny <cr.chappell@pm.me>
2025-12-07 14:30:29 -0500

    Day 64 - Set up the dir for work and laid out the 3 tasks to complete. Day 63's requirements file was also exported.

---
+++ Day_64/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import os
+
+# Due to my setup, make sure our CWD is the current dir
+if os.getcwd() != os.path.dirname(__file__):
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    print("Cwd was changed to the current file dir __file__")
+
+# TODO: Create the database, make sure to only do this if it does not exist
+# TODO: Create the table if it does not exist
+# TODO: Add movie 1 if it does not exist
+# TODO: Add movie 2 if it does not exist
+
+"""
+Movie 1 info:
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+
+Movie 2 info:
+second_movie = Movie(
+    title="Avatar The Way of Water",
+    year=2022,
+    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    rating=7.3,
+    ranking=9,
+    review="I liked the water.",
+    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+)
+
+
+"""
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DB
+
+
+# CREATE TABLE
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
