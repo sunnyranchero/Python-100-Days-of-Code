c73d73378bdd4af3826214977f121c91ea426e47
Sunny <cr.chappell@pm.me>
2025-12-29 15:47:48 -0500

    Day 67 - Completed the read all posts and the select a single post for reading. I opted for the direct param vs the ?post_id method to retrieve the id from the url.

--- Day_67_capstone_pt_3/main.py
+++ Day_67_capstone_pt_3/main.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Text, select, update, desc
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
+import os
+from rich import print as rp
 
 '''
 Make sure the required packages are installed: 
@@ -25,6 +27,14 @@ This will install the packages from the requirements.txt for this project.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+# Ensure this is running in the intended directory.
+cwd = os.getcwd()
+proj_dir = os.path.dirname(__file__)
+
+if cwd != proj_dir:
+    os.chdir(proj_dir)
+    rp("Changed the directory to the project directory.")
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
@@ -51,15 +61,28 @@ with app.app_context():
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
-    posts = []
+    # This grabs all post objects from the db and converts them to a list
+    # of objects. I added the descending order so new posts will be on top.
+    all_posts_scalar_obj = db.session.execute(select(
+        BlogPost).order_by(desc(BlogPost.id))).scalars().all()
+    posts = [post for post in all_posts_scalar_obj]
+
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    # Retrieve a BlogPost from the database based on the post_id
+    # I opted to change the endpoint instead of passing the param using:
+    #   ?Post_id=n
+    # This way I can force it to contain an integer.
+    # I also kept both ways to query the db. 
+    # I kept the 404 method for simplicity.
+
+    # requested_post = db.session.execute(
+    #     select(BlogPost).where(BlogPost.id == post_id)).scalar()
+    
+    requested_post = db.get_or_404(entity=BlogPost, ident=post_id)
+    
     return render_template("post.html", post=requested_post)
 
 
