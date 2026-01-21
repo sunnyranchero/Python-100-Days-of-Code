90f5e231932797fa92fe2d52a87d84327a6d344c
Sunny <cr.chappell@pm.me>
2026-01-21 15:02:05 -0500

    Day 71 - add Procfile for gunicorn app support

--- Day_71/main.py
+++ Day_71/main.py
@@ -12,24 +12,22 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
+import os
+from rich import print as rp
+from dotenv import load_dotenv
 
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
+cwd = os.getcwd()
+pd = os.path.dirname(__file__)
+if cwd != pd:
+    rp("[green]<<<<<<<< cwd has been changed to pd >>>>>>>>>[/green]")
+    os.chdir(pd)
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
+# Load in the file based env variables
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +54,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
